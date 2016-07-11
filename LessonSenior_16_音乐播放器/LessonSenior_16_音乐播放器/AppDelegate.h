b46ffc818e79c5d40a76a58bd7f2e1c519a87862
XW <867901476@qq.com>
2016-07-11 14:38:37 +0800

    初始化工程文件

---
+++ LessonSenior_16_音乐播放器/LessonSenior_16_音乐播放器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LessonSenior_16_音乐播放器
+//
+//  Created by lanou3g on 16/4/25.
+//  Copyright © 2016年 邢晓伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LessonSenior_16_音乐播放器/LessonSenior_16_音乐播放器/MusicInfo.m
@@ -0,0 +1,18 @@
+//
+//  MusicInfo.m
+//  LessonSenior_16_AVAudioPlayer
+//
+//  Created by lanou3g on 16/4/25.
+//  Copyright © 2016年 邢晓伟. All rights reserved.
+//
+
+#import "MusicInfo.h"
+
+@implementation MusicInfo
+
+- (NSString *)description
+{
+    return [NSString stringWithFormat:@"%@", _musicName];
+}
+
+@end
